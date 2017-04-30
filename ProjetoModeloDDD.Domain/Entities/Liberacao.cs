946bcc4bc3163f454e8ffa78ea168f6c1c50fabd
Torkzor <jorgearthurs@hotmail.com>
2017-04-30 15:23:50 -0300

    Primeiro Commit 30/04

---
+++ ProjetoModeloDDD.Domain/Entities/Liberacao.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace ProjetoModeloDDD.Domain.Entities
+{
+    public class Liberacao
+    {
+        public int LiberacaoId { get; set; }
+        public string NumeroLiberacao { get; set; }
+        public int QuantidadeTotal { get; set; }
+        public int QuantidadeRealizada { get; set; }
+
+
+    }
+}
--- ProjetoModeloDDD.Domain/Entities/Paciente.cs
+++ ProjetoModeloDDD.Domain/Entities/Paciente.cs
@@ -4,6 +4,12 @@ namespace ProjetoModeloDDD.Domain.Entities
     public class Paciente
     {
         public int PacienteId { get; set; }
+        public string NomePaciente { get; set; }
+        public string TelefonePaciente1 { get; set; }
+        public string TelefonePaciente2 { get; set; }
+        public string CarteirinhaPaciente { get; set; }
+        public bool CopartPaciente { get; set; }
+
 
     }
 }
